f27bd47bd26edb761237a9d625031a41ce108d4e
notebookmember <145191328+notebookmembers@users.noreply.github.com>
2023-09-16 16:25:37 +0530

    name changes of intro and somesome

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 const jsonData = JSON.stringify(Object);
 
 const app = express();
