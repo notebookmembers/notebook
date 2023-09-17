b05f48bcb1c92c64727753dfcff3e4c2533cf475
notebookmember <145191328+notebookmembers@users.noreply.github.com>
2023-09-17 16:49:23 +0530

    1st year link to c and o cycle

--- index.js
+++ index.js
@@ -33,16 +33,23 @@ app.get("/2nd_4th_sem_cs", function (req, res) {
   res.render("2nd/4th_sem/cs.ejs");
 });
 //------------------------------------------------post---------------------------------------------------
+// ----------------------1st year---------------------------------
+app.get("/c_cycle", function (req, res) {
+  res.render("1st/ccycle.ejs");
+});
+app.get("/p_cycle", (req, res) => {
+  res.render("1st/pcycle.ejs");
+});
 const subjects = ["cs", "ec", "is", "eee", "mech", "cv", "ei"];
 
 //--------------------2 year-----------------------------------
 
 subjects.forEach((subject) => {
-  app.get(`/3rd_sem_${subject}`, function (req, res) {
+  app.get(`/3rd_sem_${subject}`, (req, res) => {
     res.render(`2nd/3rd_sem/${subject}.ejs`);
   });
 
-  app.get(`/4th_sem_${subject}`, function (req, res) {
+  app.get(`/4th_sem_${subject}`, (req, res) => {
     res.render(`2nd/4th_sem/${subject}.ejs`);
   });
 });
@@ -50,11 +57,11 @@ subjects.forEach((subject) => {
 //------------------3 year------------------------------------
 
 subjects.forEach((subject) => {
-  app.get(`/5th_sem_${subject}`, function (req, res) {
+  app.get(`/5th_sem_${subject}`, (req, res) => {
     res.render(`3rd/5th_sem/${subject}.ejs`);
   });
 
-  app.get(`/6th_sem_${subject}`, function (req, res) {
+  app.get(`/6th_sem_${subject}`, (req, res) => {
     res.render(`3rd/6th_sem/${subject}.ejs`);
   });
 });
@@ -62,11 +69,11 @@ subjects.forEach((subject) => {
 // -----------------------4th year ----------------------
 
 subjects.forEach((subject) => {
-  app.get(`/7th_sem_${subject}`, function (req, res) {
+  app.get(`/7th_sem_${subject}`, (req, res) => {
     res.render(`4th/7th_sem/${subject}.ejs`);
   });
 
-  app.get(`/8th_sem_${subject}`, function (req, res) {
+  app.get(`/8th_sem_${subject}`, (req, res) => {
     res.render(`4th/8th_sem/${subject}.ejs`);
   });
 });
